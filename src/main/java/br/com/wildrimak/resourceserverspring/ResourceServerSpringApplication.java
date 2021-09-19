2c7edd096e5ac837e8ad5ffc7158448db4e363d6
Wildrimak <finalwildrimak@gmail.com>
2021-09-19 16:08:16 -0300

    Inicia projeto

---
+++ src/main/java/br/com/wildrimak/resourceserverspring/ResourceServerSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.resourceserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wildrimak/resourceserverspring/ResourceServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.resourceserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
